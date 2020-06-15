451d45fe8458214b33305bf4abc6cacfbe5e1479
Sara Alemanno <sara.alemanno@epfl.ch>
2020-06-15 10:24:33 +0200

    Use navigation controller in appdelegate
    Note: still does not work

--- PryvApiSwiftKitExample/AppDelegate.swift
+++ PryvApiSwiftKitExample/AppDelegate.swift
@@ -14,14 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        let mainVC = storyboard.instantiateViewController(identifier: "mainVC") as! MainViewController
+        mainVC.appId = "app-example-swift-2"
+        navigationController.pushViewController(mainVC, animated: true)
         
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let initialViewController = FactoryViewController.makeMainVC(storyboard: storyboard, appId: "app-swift-example-2")
-        // TODO: get the appId from the lauchOptions
-
-        self.window?.rootViewController = initialViewController
-        self.window?.makeKeyAndVisible()
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
 
         return true
     }
