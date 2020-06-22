82e55000a4077b8cc5ec8b739532213ed9874f05
Sara Alemanno <sara.alemanno@epfl.ch>
2020-06-22 10:43:10 +0200

    Revert "Remove strip view from storyboard"
    This reverts commit 22037c99fb24a0a4af18fff18c3060af4d81581a.

--- PryvApiSwiftKitExample/ConnectionViewController.swift
+++ PryvApiSwiftKitExample/ConnectionViewController.swift
@@ -11,28 +11,17 @@ import XLPagerTabStrip
 import PryvApiSwiftKit
 
 class ConnectionViewController: BarPagerTabStripViewController {
-    var service: Service
-    var apiEndpoint: String
-    var appId: String
-    var permissions: [String]
-    
-    init(service: Service, apiEndpoint: String, appId: String, permissions: [String]) {
-        self.service = service
-        self.apiEndpoint = apiEndpoint
-        self.appId = appId
-        self.permissions = permissions
-        
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    var service: Service?
+    var apiEndpoint: String?
+    var appId: String?
+    var permissions: [String]?
     
     override public func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
         let listVC = self.storyboard?.instantiateViewController(identifier: "connectionListVC") as! ConnectionListTableViewController
-        listVC.serviceName = service.info()?.name
-        listVC.connection = Connection(apiEndpoint: apiEndpoint)
+        listVC.serviceName = service?.info()?.name
+        if let _ = apiEndpoint {
+            listVC.connection = Connection(apiEndpoint: apiEndpoint!)
+        }
         listVC.contributePermissions = permissions
         listVC.appId = appId
         return [listVC] // TODO: add map
