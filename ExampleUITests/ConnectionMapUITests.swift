88e7bfa36754a4805dd7ce7eb8406dd5d2aaec50
Sara Alemanno <sara.alemanno@epfl.ch>
2020-07-20 16:03:07 +0200

    Update ConnectionMapUITests.swift

--- ExampleUITests/ConnectionMapUITests.swift
+++ ExampleUITests/ConnectionMapUITests.swift
@@ -70,7 +70,7 @@ class ConnectionMapUITests: XCTestCase {
         XCTAssert(app.otherElements["24.06.2020"].exists)
         
         app.segmentedControls["filterController"].buttons["Month"].tap()
-        sleep(1)
+        sleep(3)
         XCTAssert(app.otherElements["29.06.2020"].exists)
     }
     
@@ -88,6 +88,7 @@ class ConnectionMapUITests: XCTestCase {
         XCTAssert(app.otherElements["29.06.2020"].exists)
         
         changeDate(month: "May")
+        sleep(1)
         XCTAssertFalse(app.otherElements["29.06.2020"].exists)
     }
     
